abc2fa6f3803b3431b4f64b19d16abcf7e9ca83b
Max DeSantis - Pi <desantis.max.a@gmail.com>
2021-04-03 22:34:56 +0000

    pub update

--- catkin_ws/src/image_handling/src/camera_pub.cpp
+++ catkin_ws/src/image_handling/src/camera_pub.cpp
@@ -3,10 +3,13 @@
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <sensor_msgs/image_encodings.h>
 #include <iostream>
+#include <vector>
  
 // Author: Addison Sears-Collins
 // Website: https://automaticaddison.com
@@ -14,6 +17,7 @@
 // Date: June 27, 2020
  
 using namespace cv;
+using namespace std;
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "video_pub_cpp");
@@ -31,15 +35,18 @@ int main(int argc, char** argv)
     image_transport::ImageTransport it(nh);
      
     // Publish to the /camera topic
-    image_transport::Publisher pub_frame = it.advertise("camera", 1);
+    image_transport::Publisher pub_frame = it.advertise("cam/mono", 1);
+    image_transport::Publisher pub_circle_frame = it.advertise("cam/circle", 1);
     //image_transport::Publisher pub_frame_mask = it.advertise("hsv", 1);
      
     cv::Mat frame;//Mat is the image class defined in OpenCV
     sensor_msgs::ImagePtr msg;
- 
+    sensor_msgs::ImagePtr circleMsg;
     ros::Rate loop_rate(10);
     cv::Mat hsv;
-
+    vector<Vec3f> circles;
+    cv::Mat circleImg;
+    cv::Mat grayFrame;
 
     while (nh.ok()) {
  
@@ -51,22 +58,38 @@ int main(int argc, char** argv)
         ROS_ERROR_STREAM("Failed to capture image!");
         ros::shutdown();
       }
+      GaussianBlur(frame, frame, Size(3, 3), 0);
       Mat mask1, mask2;
+
+
+      cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
       // Convert image from cv::Mat (OpenCV) type to sensor_msgs/Image (ROS) type and publish
       cvtColor(frame, hsv, COLOR_BGR2HSV);
 
       inRange(hsv, Scalar(170, 120, 70), Scalar(180, 255, 255), mask1); //attempting to isolate red
-      //erode(mask1, mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-      //dilate(mask1, mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+      erode(mask1, mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+      dilate(mask1, mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
       inRange(hsv, Scalar(0, 120, 70), Scalar(10, 255, 255), mask2);
-      //erode(mask2, mask2, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-      //dilate(mask2, mask2, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+      erode(mask2, mask2, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+      dilate(mask2, mask2, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
       mask1 = mask1 + mask2;
+
+      HoughCircles(mask1, circles, HOUGH_GRADIENT, 1, mask1.rows/16, 100, 30, 0, 60);
+
+      for(Vec3i c : circles) {
+        Point center = Point(c[0], c[1]);
+        circle(frame, center, 1, Scalar(0, 100, 100), 3, LINE_AA);
+        int radius = c[2];
+        circle(frame, center, radius, Scalar(255, 0, 255), LINE_AA);
+      }
+
       msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", mask1).toImageMsg();
-      
+      circleMsg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+
       pub_frame.publish(msg);
+      pub_circle_frame.publish(circleMsg);
       cv::waitKey(1); // Display image for 1 millisecond
  
       ros::spinOnce();
